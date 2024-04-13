4358e5f90c94d3213e2a6526231064be7d4c7513
vladislav <velocrosss@yandex.ru>
2024-04-13 18:19:11 +0300

    добавлен обработчик команды на то

--- tracker_bot/handlers/state_handler.py
+++ tracker_bot/handlers/state_handler.py
@@ -2,14 +2,28 @@ from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from handlers.callback_handler import tracker_client
 from keybords.inline_keyboard import send_task_button
-from state.repair_state import RepairState, UserState,ServiceState
+from state.repair_state import RepairState, UserState, ServiceState
 
 state_router = Router()
 
+
+@state_router.message(ServiceState.user_text)
+async def cmd_phone_number(message: Message, state: FSMContext):
+    await state.update_data(user_text=message.text)
+    await state.set_state(UserState.phone_number)
+    await message.answer(f"Как с вами связаться? (телефон/ссылка на телеграм)")
+
+
+@state_router.message(ServiceState.auto_model)
+async def cmd_repair_text(message: Message, state: FSMContext):
+    await state.update_data(auto_model=message.text)
+    await state.set_state(ServiceState.user_text)
+    await message.answer(f"Что хотете поменять?")
+
+
 # Repair
-@state_router.message(RepairState.auto_model)
+@state_router.message(ServiceState.auto_model)
 async def cmd_repair_text(message: Message, state: FSMContext):
     await state.update_data(auto_model=message.text)
     await state.set_state(RepairState.user_text)
@@ -19,7 +33,7 @@ async def cmd_repair_text(message: Message, state: FSMContext):
 @state_router.message(RepairState.user_text)
 async def cmd_phone_number(message: Message, state: FSMContext):
     await state.update_data(user_text=message.text)
-    await state.set_state(RepairState.phone_number)
+    await state.set_state(UserState.phone_number)
     await message.answer(f"Как с вами связаться? (телефон/ссылка на телеграм)")
 
 
@@ -30,8 +44,8 @@ async def cmd_phone_number(message: Message, state: FSMContext):
 
     data = await state.get_data()
     await message.answer(f"Итак, ваша заявка\n"
-                            f"Марка авто: {data['auto_model']}\n"
-                            f"Текст обращения: {data['user_text']}\n"
-                            f"Контакт для связи: {data['phone_number']}\n\n"
-                            f"все верно?",reply_markup=send_task_button
-                            )
+                         f"Марка авто: {data['auto_model']}\n"
+                         f"Текст обращения: {data['user_text']}\n"
+                         f"Контакт для связи: {data['phone_number']}\n\n"
+                         f"все верно?", reply_markup=send_task_button
+                         )
